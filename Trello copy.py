f9692eb5b10b7cab25163698d48fea3bdf88be59
Louie Wong <Louie.Wong@decisivedata.net>
2018-01-30 11:44:01 -0800

    a

--- Trello copy.py
+++ Trello copy.py
@@ -4,11 +4,31 @@ Created on Fri Jan 12 11:03:28 2018
 
 @author: louie.wong
 """
-import Trello_credentials
 from trello import TrelloClient
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
-#https://api.trello.com/1/boards/lr4kcjux/cards
-#https://api.trello.com/1/actions/lr4kcjux
+
+#DD Ping Pong to dataframe 
 df = pd.read_json('https://api.trello.com/1/boards/lr4kcjux/cards')
+df = df[['id','idList', 'name','url','dateLastActivity']]
+
+#Find created date
+time = []
+for i in range(len(df)):
+    time.append(datetime.fromtimestamp(int(df.id[i][0:8], 16)))
+    
+df['time'] = time
+
+#BoardList to dataframe
+Rank = df[df.idList == '5913a83766152518bcb603c2']
+Unrank = df[df.idList == '5913a90e3f59c15f91ecdfce']
+Challenge = df[df.idList == '5914c4fdfa425a62a5cbc3ec']
+History = df[df.idList == '5914c54af867d23f762aa977']
+
+#Date Cleansing 
+
+History['Challenger'] = History.name.str.split(' ').str[0]
+History['Opponent']
+History['Score'] = History.name.str.split(':').str[1]
